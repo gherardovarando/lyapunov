9c01cfabe3651e54b83efc99be14c47ef056c262
gherardovarando <gherardo.varand@gmail.com>
2019-05-20 11:50:38 +0200

    [upd] add more return

--- R/clyap.R
+++ R/clyap.R
@@ -5,6 +5,8 @@
 #' @param A Square matrix
 #' @param Q Square matrix
 #' @param E Square matrix or \code{NULL}
+#' @param WKV the working vector obtained by a first call to lyapunov solver
+#' @param all logical
 #' 
 #' @return The solution matrix \eqn{X}.
 #' 
@@ -17,12 +19,21 @@
 #' @export
 #' @useDynLib lyapunov
 
-clyap <- function(A, Q, E=NULL) {
+clyap <- function(A, Q, E=NULL, all = FALSE) {
   if (is.null(E)) {
     E <- diag(1, NCOL(A))
   }
-  sylvester_c(A = A, E = E, QQ = Q, cond.number = FALSE)
+  sylvester_c(A = A, E = E, QQ = Q, cond.number = FALSE, 
+              all = all)
 }
 
+#' @rdname clyap
+#' @export
+clyap2 <- function(A, Q, WKV, E=NULL) {
+  if (is.null(E)) {
+    E <- diag(1, NCOL(A))
+  }
+  sylvester_cq(A = A, E = E, QQ = Q, WKV = WKV, cond.number = FALSE)
+}
 
 
